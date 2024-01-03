55346e63c58386cbab643e408cfbc2315b883c79
Sahas Subramanian <sahas.subramanian@proton.me>
2024-01-03 16:28:49 +0100

    Expose public parts of the client API in __init__.py
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- py/frequenz/client/weather/__init__.py
+++ py/frequenz/client/weather/__init__.py
@@ -2,3 +2,8 @@
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
 """The Weather Forecast API client."""
+
+from ._client import Client
+from ._types import ForecastFeature, Forecasts, Location
+
+__all__ = ["Client", "ForecastFeature", "Forecasts", "Location"]
